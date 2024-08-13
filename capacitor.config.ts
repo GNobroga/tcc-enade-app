16e4150ba560f2f71ea03b2711c7ed4884d296e5
Gabiroba <gabrielcardosogirarde@gmail.com>
2024-08-13 05:40:24 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tcc-enade-app',
+  webDir: 'www'
+};
+
+export default config;
