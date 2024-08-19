f9d3c81103491feda23b09728afed22a35ef6d9e
gabiroba <gabrielcardosogirarde@gmail.com>
2024-08-19 19:40:37 -0300

    style: Ajuste nas estilização

--- app-frontend/src/app/feature/login/pages/login-page/login-page.component.ts
+++ app-frontend/src/app/feature/login/pages/login-page/login-page.component.ts
@@ -1,10 +1,28 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { AnimationController } from '@ionic/angular';
+import { AnimationController, LoadingController } from '@ionic/angular';
 import { LoginFormComponent } from '../../components/login-form/login-form.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss'],
 })
-export class LoginPageComponent  {}
+export class LoginPageComponent {
+
+  constructor(
+    readonly loadingController: LoadingController,
+    readonly router: Router
+  ) {
+
+  }
+
+  public async showLoading() {
+    const loading = await this.loadingController.create({
+      duration: 1000,
+    });
+
+    await loading.present();
+    this.router.navigate(['/tabs']);
+  }
+}
--- app-frontend/src/app/feature/tab/pages/study-page/study-page.component.ts
+++ app-frontend/src/app/feature/tab/pages/study-page/study-page.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import { Animation, AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-study-page',
   templateUrl: './study-page.component.html',
   styleUrls: ['./study-page.component.scss'],
 })
-export class StudyPageComponent {
+export class StudyPageComponent implements AfterViewInit {
 
-  constructor(readonly router: Router) { }
+  @ViewChildren('studyItem')
+  public readonly studyItems!: QueryList<ElementRef>;
 
-  
+  constructor(
+    public readonly animationController: AnimationController
+  ) {
+
+  }
+
+  public ngAfterViewInit() {
+      this.studyItems.map(item => item.nativeElement)
+      .forEach((element, position) => {
+          this.createAnimation(element, (1 + position) * 150).play();
+      });
+  }
+
+  public createAnimation(element: HTMLElement, delay: number) {
+    return this.animationController.create()
+    .addElement(element)
+    .fromTo('opacity', '0', '1')
+    .easing('ease')
+    .duration(1000)
+    .delay(delay)
+    .fill('forwards');
+  }
 
 }
